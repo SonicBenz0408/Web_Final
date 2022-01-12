a45405f4543b24bc3e8e3fd3acc464282fafb34e
Benz <ben1234560408@gmail.com>
2022-01-12 18:13:42 +0800

    db

--- backend/wssConnect.js
+++ backend/wssConnect.js
@@ -50,37 +50,31 @@ const iconData = async (ws) => {
 
     var allData = { "Hololive": [], "彩虹社": [], "其他": [] }
 
-    await Icon.find({'corp': "Hololive"}, "-_id -__v").exec((err, res) => {
-        if(err) throw err
-        for(let i=0 ; i < res.length ; i++){
-            let name = {}
-            name[res[i]["name"]] = [res[i]["icon"], res[i]["url"], res[i]["corp"]]
-            allData["Hololive"].push(name)
-        }
-    })
+    var dbHoloIcon = await Icon.find({'corp': "Hololive"}, "-_id -__v")
+    
+    for(let i=0 ; i < dbHoloIcon.length ; i++){
+        let name = {}
+        name[dbHoloIcon[i]["name"]] = [dbHoloIcon[i]["icon"], dbHoloIcon[i]["url"], dbHoloIcon[i]["corp"]]
+        allData["Hololive"].push(name)
+    }
 
-    await Icon.find({'corp': "彩虹社"}, "-_id -__v").exec((err, res) => {
-        if(err) throw err
-        for(let i=0 ; i < res.length ; i++){
-            let name = {}
-            name[res[i]["name"]] = [res[i]["icon"], res[i]["url"], res[i]["corp"]]
-            allData["彩虹社"].push(name)
-        }
-    })
+    var dbNijiIcon = await Icon.find({'corp': "彩虹社"}, "-_id -__v")
+    
+    for(let i=0 ; i < dbHoloIcon.length ; i++){
+        let name = {}
+        name[dbNijiIcon[i]["name"]] = [dbNijiIcon[i]["icon"], dbNijiIcon[i]["url"], dbNijiIcon[i]["corp"]]
+        allData["彩虹社"].push(name)
+    }
 
-    await Icon.find({'corp': "其他"}, "-_id -__v").exec((err, res) => {
-        if(err) throw err
-        for(let i=0 ; i < res.length ; i++){
-            let name = {}
-            name[res[i]["name"]] = [res[i]["icon"], res[i]["url"], res[i]["corp"]]
-            allData["其他"].push(name)
+    var dbOtherIcon = await Icon.find({'corp': "其他"}, "-_id -__v")
+    
+    for(let i=0 ; i < dbOtherIcon.length ; i++){
+        let name = {}
+        name[dbOtherIcon[i]["name"]] = [dbOtherIcon[i]["icon"], dbOtherIcon[i]["url"], dbNijiIcon[i]["corp"]]
+        allData["其他"].push(name)
+    }
 
-            if(i === res.length - 1){
-                // console.log(upstream);
-                sendData(["icon", [{ allData }]], ws)
-            }
-        }
-    })
+    sendData(["icon", [{ allData }]], ws)
 
 }
 
