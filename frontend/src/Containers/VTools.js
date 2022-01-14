741099008c1edb3fe358f32883c5fc7d90e9a715
Benz <ben1234560408@gmail.com>
2022-01-14 23:02:09 +0800

    wss

--- frontend/src/Containers/VTools.js
+++ frontend/src/Containers/VTools.js
@@ -47,7 +47,7 @@ const VTools = () => {
     
     useEffect(() => {
         
-        ws.current = new WebSocket("ws://vtdd.herokuapp.com/:4001")
+        ws.current = new WebSocket("wss://vtdd.herokuapp.com/:4001")
         //ws.current = new WebSocket("ws://localhost:4000")
         ws.current.onopen = async () => {
             console.log("connected")
